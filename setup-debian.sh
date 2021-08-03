c10d676c03603f0ee311e35ddf9896a6fbcaea84
Muhamad Aji Pandean Mertayasa <37402263+ajipandean@users.noreply.github.com>
2021-08-03 13:31:53 +0800

    Update setup-debian.sh

--- setup-debian.sh
+++ setup-debian.sh
@@ -54,7 +54,6 @@ apt install brave-browser
 echo 'Brave Browser installed'
 
 # Install Chrome
-cd ~/Downloads
 echo 'Downloading Google Chrome...'
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 echo 'Installing Google Chrome...'
