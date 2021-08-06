7930649d55d88c1457c9aed8bac464a86b54aaf7
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2021-08-06 22:14:29 +0800

    added installation of pgadmin4

--- install.sh
+++ install.sh
@@ -11,6 +11,11 @@ sudo apt update
 sudo apt install brave-browser -y
 echo "installing brave-browser...done\n"
 
+# install pgadmin4
+sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
+sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+sudo apt install pgadmin4-desktop
+
 # install vim
 sudo apt install vim -y
 echo "installing vim...done\n"
